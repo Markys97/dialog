5b98074edd8abbf33ae4155ab20def05e249084a
Markys97 <70927348+Markys97@users.noreply.github.com>
2023-02-02 19:04:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 
 let burger_btn= document.querySelector('.header__burger');
 burger_btn.addEventListener('click',function(e){
- console.log(
-    this
+
+  this
  .closest('.header')
  .classList.toggle('active')
- )
+ document.body.classList.toggle('body-off')  
+ 
 })
